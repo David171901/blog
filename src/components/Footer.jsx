de84d0ec4d78fc6c6491bd551fa7ad64de14a45a
david.pino <david@ip-172-31-89-46.ec2.internal>
2023-05-07 02:45:59 +0000

    build: firts commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import React from "react";
+import Layout from "./Layout";
+import { motion } from "framer-motion";
+import { PayPalIcon } from "./Icons";
+
+const Footer = () => {
+  return (
+    <footer
+      className="w-full border-t-2 border-solid border-dark
+    font-medium text-lg dark:text-light dark:border-light sm:text-base
+    "
+    >
+      <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
+        <span>{new Date().getFullYear()} &copy; Reservados todos los derechos.</span>
+        <div className="flex items-center lg:py-2">
+          Apoyame <span className="text-primary dark:text-primaryDark text-2xl px-1">&#9825;</span>
+          por&nbsp;
+          <Link
+            href="https://devdreaming.com"
+            className="underline
+            underline-offset-2
+            "
+            target={"_blank"}
+          >
+            PayPal
+          </Link>
+        </div>
+      </Layout>
+    </footer>
+  );
+};
+
+export default Footer;
